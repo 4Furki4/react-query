0970ad6dc2c384c88e449040acaf1e078ac46fd8
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-06-11 18:50:35 +0300

    add: axios instance with baseUrl created in api.js

--- src/methods/posts.js
+++ src/methods/posts.js
@@ -1,12 +1,8 @@
-import axios from "axios"
-const BASE_URL = 'https://jsonplaceholder.typicode.com/'
-
+import api from './api'
 export function getPosts() {
-    return axios.get(`${BASE_URL}posts`).then(response => response.data)
+    return api.get('/posts').then(response => response.data)
 }
 
 export function getPost(id) {
-    return fetch(`${BASE_URL}posts/${id}`)
-        .then(response => response.json())
-        .then(json => json)
+    return api.get(`/posts/${id}`).then(response => response.data)
 }
